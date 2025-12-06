2836baefaf6bac82259ea86647845be5792327e1
strawrimp <strawrimp@gmail.com>
2025-12-07 01:13:56 +0900

    Fix format error

--- utils.py
+++ utils.py
@@ -25,6 +25,7 @@ def extract_audio(youtube_url, output_path="downloads"):
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
         },
         'nocheckcertificate': True,
+        'ignoreerrors': True,
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
